dd80945a0424d7f9cc1d3992f9d158d5a1624c43
cutajarj <james.cutajar@gmail.com>
2021-08-03 07:51:07 +0100

    Joins in Java

---
+++ src/main/java/com/cutajarjames/multithreading/join/FileSearch.java
@@ -0,0 +1,37 @@
+package com.cutajarjames.multithreading.join;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSearch {
+    List<File> matches = new ArrayList<>();
+
+    public void find(File root, String filename) {
+        System.out.println("Searching in " + root.getAbsolutePath());
+        var childThreads = new ArrayList<Thread>();
+        for (var f: root.listFiles()) {
+            if (f.getName().contains(filename)) {
+                matches.add(f);
+            }
+            if (f.isDirectory()) {
+                var t = new Thread(() -> find(f, filename));
+                childThreads.add(t);
+                t.start();
+            }
+        }
+        for (var t: childThreads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        var fileSearch = new FileSearch();
+        fileSearch.find(new File("c:/tools"), "README.md");
+        fileSearch.matches.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/cutajarjames/multithreading/join/SimpleJoinExample.java
@@ -0,0 +1,24 @@
+package com.cutajarjames.multithreading.join;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleJoinExample extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Child thread doing work");
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Child thread finished work");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var childThread = new SimpleJoinExample();
+        childThread.start();
+        System.out.println("Parent Thread is waiting...");
+        childThread.join();
+        System.out.println("Parent Thread unblocked...");
+    }
+}
