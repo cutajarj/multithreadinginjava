af1246c3a8e21f3c75d395b3f8202c4034e2a211
cutajarj <james.cutajar@gmail.com>
2021-08-26 13:46:16 +0100

    Spinlock implementation

---
+++ src/main/java/com/cutajarjames/multithreading/spinlock/SpinLock.java
@@ -0,0 +1,43 @@
+package com.cutajarjames.multithreading.spinlock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class SpinLock implements Lock {
+    //AtomicInteger locked = new AtomicInteger(0);
+    AtomicBoolean locked = new AtomicBoolean(false);
+
+    @Override
+    public void lock() {
+        while (!locked.compareAndSet(false, true)) {
+            Thread.yield();
+        }
+    }
+
+    @Override
+    public void unlock() {
+        locked.set(false);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
